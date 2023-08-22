0862eb87a580e9356e76ead5eb05eea6fb330f67
Artur Buja <artur.buja2@gmail.com>
2023-08-22 15:31:00 +0200

    fetching post on homepage

--- lib/actions/thread.actions.ts
+++ lib/actions/thread.actions.ts
@@ -36,3 +36,37 @@ export async function createThread({
     throw new Error(`Faild to create thread: ${error.message}`);
   }
 }
+
+export async function fetchPosts(pageNumer = 1, pageSize = 20) {
+  connectToDatabase();
+
+  const skipAmount = (pageNumer - 1) * pageSize;
+  try {
+    // Fetch the posts without parents
+    const postsQuery = Thread.find({
+      parentId: { $in: [null, undefined] },
+    })
+      .sort({ createdAt: 'desc' })
+      .skip(skipAmount)
+      .limit(pageSize)
+      .populate({ path: 'author', model: User })
+      .populate({
+        path: 'children',
+        populate: {
+          path: 'author',
+          model: User,
+          select: '_id name parentId image',
+        },
+      });
+
+    const totalPostCount = await Thread.countDocuments({
+      parentId: { $in: [null, undefined] },
+    });
+    const posts = await postsQuery.exec();
+
+    const isNext = totalPostCount > skipAmount + posts.length;
+    return { posts, isNext };
+  } catch (error: any) {
+    throw new Error(`Faild to fetch posts: ${error.message}`);
+  }
+}
